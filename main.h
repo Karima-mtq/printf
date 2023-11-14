a7a3ef128661c6816ab39ee9c9c53d2cba581e92
karima <karima.mtq@gmail.com>
2023-11-14 04:21:22 -0800

    printf

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_H
+#define main_H
+
+/*
+ * File: main.h
+ * auth: me
+ * Time: Now
+ * 	used in this directory
+ */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
+
